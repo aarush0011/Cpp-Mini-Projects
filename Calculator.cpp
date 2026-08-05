752667ebb37bc8c39f5ad89ba2f137bba93879e9
Aarush Reddy <aarushmreddy005@gmail.com>
2026-08-05 20:30:00 +0530

    2nd number added

--- Calculator.cpp
+++ Calculator.cpp
@@ -5,13 +5,15 @@ int main() {
     char op;
     double num1, num2, result;
 
-    std::cout<<"Enter your first number: ";
+    std::cout<<"Enter your 1st number: ";
     std::cin >> num1;
 
     std::cout<<"Enter your Operator(+, -, * or /): ";
-    std:: cin >> op;
+    std::cin >> op;
 
-    
+    std::cout<<"Enter your 2nd number: ";
+    std::cin >> num2;
+     
 
     return 0;
 }
