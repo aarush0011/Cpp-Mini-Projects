a9db9c560fd7bc24ad765e7f2ad7f4244c19dcaa
Aarush Reddy <aarushmreddy005@gmail.com>
2026-08-05 20:28:57 +0530

    operator

--- Calculator.cpp
+++ Calculator.cpp
@@ -7,6 +7,10 @@ int main() {
 
     std::cout<<"Enter your first number: ";
     std::cin >> num1;
+
+    std::cout<<"Enter your Operator(+, -, * or /): ";
+    std:: cin >> op;
+
     
 
     return 0;
