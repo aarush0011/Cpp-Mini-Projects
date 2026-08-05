db07f66013428a84253469fc4778bcd9f6ea7562
Aarush Reddy <aarushmreddy005@gmail.com>
2026-08-05 20:27:25 +0530

    1st number

---
+++ Calculator.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main() {
+    
+    char op;
+    double num1, num2, result;
+
+    std::cout<<"Enter your first number: ";
+    std::cin >> num1;
+    
+
+    return 0;
+}
