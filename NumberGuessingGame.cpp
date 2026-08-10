5c4edea313a20a5a94e1939a85f890625dff475c
Aarush Reddy <aarushmreddy005@gmail.com>
2026-08-10 20:08:27 +0530

    Game structure

--- NumberGuessingGame.cpp
+++ NumberGuessingGame.cpp
@@ -9,7 +9,14 @@ int main() {
     srand(time(NULL));
     num = rand() % 100 + 1;
 
-    cout<<"Number Guessing Game";
+    cout<<"Number Guessing Game\n";
+
+    do{
+        cout<<"Enter a guess between 1-100: ";
+        cin>>guess;
+        tries++;
+    }while(guess != num);
+
 
     return 0;
 }
