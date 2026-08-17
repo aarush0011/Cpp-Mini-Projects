4f060a8b430556250fae4132293d53beabd52cc2
Aarush Reddy <aarushmreddy005@gmail.com>
2026-08-17 12:27:33 +0530

    Showing choices

--- RPS.cpp
+++ RPS.cpp
@@ -6,8 +6,8 @@ char userChoice(){
     char player;
     cout<<"Rock-Paper-Scissors Game\n";
     do{
-        cout<<"Choose one: ";
-        cout<<"'r' - Rock\n'p' - Paper\n's' - Scissors: ";
+        cout<<"Choose one: \n";
+        cout<<"'r' - Rock\n'p' - Paper\n's' - Scissors\n";
         cin >> player;
     }while(player != 'r' && player != 'p' && player != 's');
 
@@ -18,7 +18,22 @@ char compChoice(){
     return 0;
 }
 
-void showChoice(char choice){}
+void showChoice(char choice){
+
+    switch(choice){
+        case 'r':
+        cout<<"Rock\n";
+        break;
+
+        case 'p':
+        cout<<"Paper\n";
+        break;
+
+        case 's':
+        cout<<"Scissor\n";
+        break;
+    }
+}
 
 void chooseWinner(char player, char comp){}
 
@@ -27,6 +42,8 @@ int main() {
     char player, comp;
 
     player = userChoice();
+    cout<<"Your choice: ";
+    showChoice(player);
 
     return 0;
 }
