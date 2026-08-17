261640a42b8e58d72b2059875d7810e4ce8ccf87
Aarush Reddy <aarushmreddy005@gmail.com>
2026-08-17 12:22:31 +0530

    User choice

--- RPS.cpp
+++ RPS.cpp
@@ -1,13 +1,32 @@
 #include <iostream>
 using namespace std;
 
+char userChoice(){
+
+    char player;
+    cout<<"Rock-Paper-Scissors Game\n";
+    do{
+        cout<<"Choose one: ";
+        cout<<"'r' - Rock\n'p' - Paper\n's' - Scissors: ";
+        cin >> player;
+    }while(player != 'r' && player != 'p' && player != 's');
+
+    return player;
+}
+
+char compChoice(){
+    return 0;
+}
+
+void showChoice(char choice){}
+
+void chooseWinner(char player, char comp){}
+
 int main() {
 
-    string choice;
-    cout<<"Enter 'r', 'p' or 's'"<<'\n';
-    cin >> choice;
+    char player, comp;
 
-    
+    player = userChoice();
 
     return 0;
 }
